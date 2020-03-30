e8ff6566126e5e80ab356e73e739f1bad4d5005f
averdenal <amaury.y.verdenal@gmail.com>
2020-03-30 23:44:20 +0200

    etabs

--- views/viewEtab.php
+++ views/viewEtab.php
@@ -6,64 +6,262 @@
 
 <div class="grid grid-3">
     <div class="etabs_Item">
-        <figure class="etabs_Item_Cover liquid" style='background: url("public/imgs/quest/cover/01.png") center center / cover no-repeat;'>
-            <img src="public/imgs/quest/cover/01.png" alt="cover-01" style="display: none;">
+        <figure class="etabs_Item_Cover" style='background: url("public/imgs/etabs/po.jpg") center center / cover no-repeat;'>
+            <p class="text_Sticker">Player one</p>
         </figure>
-        <p class="text_Sticker">Player one</p>
         <p class="cat_Sticker">Bar</p>
-        <div class="etabs_Item etabs_Item_Info">
+        <div class="etabs_Item_Info">
             <div class="etabs_Item_Badge">
                 <img src="public/imgs/quest/completedq-b.png" alt="top Etab">
             </div>
             <div class="etabs_Status">
-                <p class="open">ouvert</p>
+                <p>Open</p>
+                <div class="etab_Open"></div>
             </div>
             <div class="etabs_Info">
-                <div>
-                    <p>224 Rue Saint-Denis</p>
-                    <p>75002 Paris</p>
+                <div class="etabs_Info_1">
+                    <p class="note">4.5</p>
+                    <p class="nb_Vote">45 votes</p>
+                    
                 </div>
-                <div>
+                <div class="etabs_Info_2">
                     <p><span style="color:#F1C40F">€ € </span>€</p>
                 </div>
                 
             </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <div class="fas fa-map-marker-alt localisation"></div>
+                    <div>
+                        <p>224 Rue Saint-Denis</p>
+                        <p>75002 Paris</p>
+                    </div>
+                </div>
+                <div class="etabs_Info_2">
+                    <div class="fas fa-phone-square-alt localisation"></div>
+                    <p>0123456789</p>
+
+                </div>
+            </div>
+            <div class="etabs_Info etabs_Info_Text">
+                <p>Tu aime l'univers game retro dans un bar qui est totalement dans le thème ?</p>
+            </div>
         </div>
     </div>
     <div class="etabs_Item">
-        <figure class="etabs_Item_Cover liquid" style='background: url("public/imgs/quest/cover/01.png") center center / cover no-repeat;'>
-            <img src="public/imgs/quest/cover/01.png" alt="cover-01" style="display: none;">
+        <figure class="etabs_Item_Cover" style='background: url("public/imgs/etabs/po.jpg") center center / cover no-repeat;'>
+            <p class="text_Sticker">Player one</p>
         </figure>
-        <p class="text_Sticker">Trucmush</p>
         <p class="cat_Sticker">Bar</p>
-        <div class="etabs_Item etabs_Item_Info">
+        <div class="etabs_Item_Info">
             <div class="etabs_Item_Badge">
                 <img src="public/imgs/quest/completedq-b.png" alt="top Etab">
+            </div>
+            <div class="etabs_Status">
+                <p>Open</p>
+                <div class="etab_Open"></div>
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <p class="note">4.5</p>
+                    <p class="nb_Vote">45 votes</p>
+                    
+                </div>
+                <div class="etabs_Info_2">
+                    <p><span style="color:#F1C40F">€ € </span>€</p>
+                </div>
+                
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <div class="fas fa-map-marker-alt localisation"></div>
+                    <div>
+                        <p>224 Rue Saint-Denis</p>
+                        <p>75002 Paris</p>
+                    </div>
+                </div>
+                <div class="etabs_Info_2">
+                    <div class="fas fa-phone-square-alt localisation"></div>
+                    <p>0123456789</p>
+
+                </div>
+            </div>
+            <div class="etabs_Info etabs_Info_Text">
+                <p>Tu aime l'univers game retro dans un bar qui est totalement dans le thème ?</p>
             </div>
         </div>
     </div>
     <div class="etabs_Item">
-        <figure class="etabs_Item_Cover liquid" style='background: url("public/imgs/quest/cover/01.png") center center / cover no-repeat;'>
-            <img src="public/imgs/quest/cover/01.png" alt="cover-01" style="display: none;">
+        <figure class="etabs_Item_Cover" style='background: url("public/imgs/etabs/po.jpg") center center / cover no-repeat;'>
+            <p class="text_Sticker">Player one</p>
         </figure>
-        <p class="text_Sticker">Reset Bar</p>
         <p class="cat_Sticker">Bar</p>
-        <div class="etabs_Item etabs_Item_Info">
+        <div class="etabs_Item_Info">
             <div class="etabs_Item_Badge">
                 <img src="public/imgs/quest/completedq-b.png" alt="top Etab">
+            </div>
+            <div class="etabs_Status">
+                <p>Open</p>
+                <div class="etab_Open"></div>
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <p class="note">4.5</p>
+                    <p class="nb_Vote">45 votes</p>
+                    
+                </div>
+                <div class="etabs_Info_2">
+                    <p><span style="color:#F1C40F">€ € </span>€</p>
+                </div>
+                
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <div class="fas fa-map-marker-alt localisation"></div>
+                    <div>
+                        <p>224 Rue Saint-Denis</p>
+                        <p>75002 Paris</p>
+                    </div>
+                </div>
+                <div class="etabs_Info_2">
+                    <div class="fas fa-phone-square-alt localisation"></div>
+                    <p>0123456789</p>
+
+                </div>
+            </div>
+            <div class="etabs_Info etabs_Info_Text">
+                <p>Tu aime l'univers game retro dans un bar qui est totalement dans le thème ?</p>
             </div>
         </div>
     </div>
     <div class="etabs_Item">
-        <figure class="etabs_Item_Cover liquid" style='background: url("public/imgs/quest/cover/01.png") center center / cover no-repeat;'>
-            <img src="public/imgs/quest/cover/01.png" alt="cover-01" style="display: none;">
+        <figure class="etabs_Item_Cover" style='background: url("public/imgs/etabs/po.jpg") center center / cover no-repeat;'>
+            <p class="text_Sticker">Player one</p>
         </figure>
-        <p class="text_Sticker">La Felicita</p>
-        <p class="cat_Sticker">Zone Mix</p>
-        <div class="etabs_Item etabs_Item_Info">
+        <p class="cat_Sticker">Bar</p>
+        <div class="etabs_Item_Info">
             <div class="etabs_Item_Badge">
                 <img src="public/imgs/quest/completedq-b.png" alt="top Etab">
             </div>
+            <div class="etabs_Status">
+                <p>Open</p>
+                <div class="etab_Open"></div>
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <p class="note">4.5</p>
+                    <p class="nb_Vote">45 votes</p>
+                    
+                </div>
+                <div class="etabs_Info_2">
+                    <p><span style="color:#F1C40F">€ € </span>€</p>
+                </div>
+                
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <div class="fas fa-map-marker-alt localisation"></div>
+                    <div>
+                        <p>224 Rue Saint-Denis</p>
+                        <p>75002 Paris</p>
+                    </div>
+                </div>
+                <div class="etabs_Info_2">
+                    <div class="fas fa-phone-square-alt localisation"></div>
+                    <p>0123456789</p>
+
+                </div>
+            </div>
+            <div class="etabs_Info etabs_Info_Text">
+                <p>Tu aime l'univers game retro dans un bar qui est totalement dans le thème ?</p>
+            </div>
         </div>
     </div>
+    <div class="etabs_Item">
+        <figure class="etabs_Item_Cover" style='background: url("public/imgs/etabs/po.jpg") center center / cover no-repeat;'>
+            <p class="text_Sticker">Player one</p>
+        </figure>
+        <p class="cat_Sticker">Bar</p>
+        <div class="etabs_Item_Info">
+            <div class="etabs_Item_Badge">
+                <img src="public/imgs/quest/completedq-b.png" alt="top Etab">
+            </div>
+            <div class="etabs_Status">
+                <p>Open</p>
+                <div class="etab_Open"></div>
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <p class="note">4.5</p>
+                    <p class="nb_Vote">45 votes</p>
+                    
+                </div>
+                <div class="etabs_Info_2">
+                    <p><span style="color:#F1C40F">€ € </span>€</p>
+                </div>
+                
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <div class="fas fa-map-marker-alt localisation"></div>
+                    <div>
+                        <p>224 Rue Saint-Denis</p>
+                        <p>75002 Paris</p>
+                    </div>
+                </div>
+                <div class="etabs_Info_2">
+                    <div class="fas fa-phone-square-alt localisation"></div>
+                    <p>0123456789</p>
+
+                </div>
+            </div>
+            <div class="etabs_Info etabs_Info_Text">
+                <p>Tu aime l'univers game retro dans un bar qui est totalement dans le thème ?</p>
+            </div>
+        </div>
+    </div>
+    <div class="etabs_Item">
+        <figure class="etabs_Item_Cover" style='background: url("public/imgs/etabs/po.jpg") center center / cover no-repeat;'>
+            <p class="text_Sticker">Player one</p>
+        </figure>
+        <p class="cat_Sticker">Bar</p>
+        <div class="etabs_Item_Info">
+            <div class="etabs_Item_Badge">
+                <img src="public/imgs/quest/completedq-b.png" alt="top Etab">
+            </div>
+            <div class="etabs_Status">
+                <p>Open</p>
+                <div class="etab_Open"></div>
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <p class="note">4.5</p>
+                    <p class="nb_Vote">45 votes</p>
+                    
+                </div>
+                <div class="etabs_Info_2">
+                    <p><span style="color:#F1C40F">€ € </span>€</p>
+                </div>
+                
+            </div>
+            <div class="etabs_Info">
+                <div class="etabs_Info_1">
+                    <div class="fas fa-map-marker-alt localisation"></div>
+                    <div>
+                        <p>224 Rue Saint-Denis</p>
+                        <p>75002 Paris</p>
+                    </div>
+                </div>
+                <div class="etabs_Info_2">
+                    <div class="fas fa-phone-square-alt localisation"></div>
+                    <p>0123456789</p>
+
+                </div>
+            </div>
+            <div class="etabs_Info etabs_Info_Text">
+                <p>Tu aime l'univers game retro dans un bar qui est totalement dans le thème ?</p>
+            </div>
+        </div>
+    </div>
+
 </div>
