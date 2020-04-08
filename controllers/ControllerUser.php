286d3260ce2bc6fa329e4ec5eae8df21c4f468fc
averdenal <amaury.y.verdenal@gmail.com>
2020-04-08 17:04:04 +0200

    page profil begin responsive

--- controllers/ControllerUser.php
+++ controllers/ControllerUser.php
@@ -3,18 +3,25 @@ namespace GOA\controllers;
 
 use GOA\controllers\BaseController;
 use GOA\models\managers\UserManager;
+use GOA\models\managers\BadgeManager;
 
 class ControllerUser extends BaseController{
     private $_userManager;
+    private $_badgeManager;
     public function __construct()
     {
         $this->_userManager = new UserManager();
+        $this->_badgeManager = new BadgeManager();
     }
-    public function getProfil($id)
+    public function getProfil($login)
     {
         $user = $this->_userManager->getConnecte();
-        $this->addParam('titlePage','Profil - '.$user->getLogin());
+        $this->addParam('titlePage','Accueil');
         $this->addParam('user',$user);
-        $this->template('viewAccueil.twig');
+        if($user != null){
+            $badges = $this->_badgeManager->getBadgesByUser($user->getId(),5);
+            $this->addParam('badges',$badges);
+        }
+        $this->template('viewProfil.twig');
     }
 }
